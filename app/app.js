ddbd2620f790dfc50207b6cb27cd150c928660da
Don <donshius@inbox.lt>
2019-11-06 13:05:11 +0000

    dynamically resizing blockly

--- app/app.js
+++ app/app.js
@@ -15,8 +15,33 @@ Blockly.Blocks['string_length'] = {
     }
   };
 
-var workspace = Blockly.inject('blocklyDiv',
+var blocklyDiv = document.getElementById('blocklyDiv');
+var blocklyArea = document.getElementById('blocklyArea');
+
+var workspace = Blockly.inject(blocklyDiv,
     {toolbox: document.getElementById('toolbox')});
+
+var onresize = function(e) {
+    // Compute the absolute coordinates and dimensions of blocklyArea.
+    var element = blocklyArea;
+    var x = 0;
+    var y = 0;
+    do {
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
+    } while (element);
+    // Position blocklyDiv over blocklyArea.
+    blocklyDiv.style.left = x + 'px';
+    blocklyDiv.style.top = y + 'px';
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    Blockly.svgResize(workspace);
+    console.log('Resizing:', blocklyArea)
+};
+window.addEventListener('resize', onresize, false);
+onresize()
+Blockly.svgResize(workspace)
 
 function updateCode() {
     console.log('Updating textarea')
