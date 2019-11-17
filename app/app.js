c76acb9da3f0ed4101cd6bfa6e4a3d71d20f7640
D.Paplauskas <dp404@kent.ac.uk>
2019-11-17 19:35:14 +0000

    implemented responsive blockly container in a better way

--- app/app.js
+++ app/app.js
@@ -2,34 +2,11 @@ var Blockly = require('blockly')
 var Dialogs = require('dialogs')()
 require('./app/blocks')(Blockly.Blocks)
 
-
 var blocklyDiv = document.getElementById('blocklyDiv');
 var blocklyArea = document.getElementById('blocklyArea');
 
-var workspace = Blockly.inject(blocklyDiv,
+var workspace = Blockly.inject('blocklyDiv',
     {toolbox: document.getElementById('toolbox')});
-
-var onresize = function(e) {
-    // Compute the absolute coordinates and dimensions of blocklyArea.
-    var element = blocklyArea;
-    var x = 0;
-    var y = 0;
-    do {
-        x += element.offsetLeft;
-        y += element.offsetTop;
-        element = element.offsetParent;
-    } while (element);
-    // Position blocklyDiv over blocklyArea.
-    blocklyDiv.style.left = x + 'px';
-    blocklyDiv.style.top = y + 'px';
-    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
-    Blockly.svgResize(workspace);
-};
-window.addEventListener('resize', onresize, false);
-onresize()
-Blockly.svgResize(workspace)
-
 
 function newProject() {
 	if (workspace.getAllBlocks().length >0){
