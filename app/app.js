d5f2c408d213927fb4975f826ba3875a131a8933
tsdh2 <tsdh2@kent.ac.uk>
2019-11-27 10:15:04 +0000

    basic file saving and loading

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var Blockly = require('blockly')
 var Dialogs = require('dialogs')()
+var {dialog} = require('electron').remote
 var FS = require('fs')
 /* Setup generator */
 // require('./generators/cpp')
@@ -15,7 +16,7 @@ Blockly.Blocks['string_length'] = {
         this.setHelpUrl("");
     }
   };
-
+var currentpath = "";
 var blocklyDiv = document.getElementById('blocklyDiv');
 var blocklyArea = document.getElementById('blocklyArea');
 
@@ -47,17 +48,21 @@ Blockly.svgResize(workspace)
 function newProject() {
 	if (workspace.getAllBlocks().length >0){
 		if (!confirm("Do You Wish to save?")) {
-			workspace.clear()
+            alert("cancelled!")
 		} else {
-			saveProject()
-		}
+            saveProject()
+            workspace.clear()
+            currentpath = "";
+        }
 	} else {
-		workspace.clear()
+        workspace.clear()
+        currentpath = "";
     }
 }
 
 //Update - Use Electron to access the File System
 //Save a file
+/** DEPRECATED by tsdh2
 function saveProject()
 {
 
@@ -69,18 +74,55 @@ function saveProject()
     node.innerHTML = xml_text;
     FS.writeFile(filename,node);
 }
+*/
 
 //Works for Singular Files
 //Loads Project into workspace
 var loadProject = function(event) {
-    var file = event.target;
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.onloadend = function() {
-        var xml = $(reader.result);
-    }
-    var xml = Blockly.Xml.textToDom(xml);
-    Blockly.Xml.domToWorkspace(xml, workspace)
+    dialog.showOpenDialog(filename => {
+        if (filename === undefined) {
+            console.log("No File selected");
+            return;
+        }
+        FS.readFile(filename[0], 'utf-8', (err, data) => {
+            if (err) {
+                console.log("something went wrong: " + err.message)
+                return;
+            }
+            var xml = Blockly.Xml.textToDom(data);
+            Blockly.Xml.domToWorkspace(xml, workspace)
+        })
+    })
+    
+}
+
+/**
+ * Save Project Using Electron File System
+ */
+function saveProject()
+{
+    var xml = Blockly.Xml.workspaceToDom(workspace);
+    var xml_text = Blockly.Xml.domToText(xml);
+    if (currentpath == "") {
+        dialog.showSaveDialog((filename) => {
+        if (filename === undefined) {
+            console.log("User pressed save but didn't do anything ");
+            return;
+        }
+        currentpath = filename;
+        FS.writeFile(filename,xml_text, (err) => {
+            if (err) {
+                console.log("error saving");
+                return;
+            }
+            else {
+                console.log("Saved!")
+                alert("Saved!")
+            }
+        });
+        })
+    } 
+
 }
 
 //Load an existing project
