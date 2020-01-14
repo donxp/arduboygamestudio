f25357af8c0c46cf672ded07225257173485d552
tsdh2 <tsdh2@kent.ac.uk>
2020-01-14 21:00:39 +0000

    Added functions

---
+++ app/components/Preferences.js
@@ -0,0 +1,11 @@
+let COMHelper = require('../util/COMHelper')
+
+VTTCue.component('preferences', {
+    template: `
+    <div>
+        <select name="COMPorts">
+        <option value="x">x</option>
+        </select>
+    </div>
+    `,//makes the template for page
+})
--- app/util/COMHelper.js
+++ app/util/COMHelper.js
@@ -1,6 +1,10 @@
 const serialport = require('serialport') //Module
 const SerialPort = serialport.SerialPort; //
 
+const board = {
+    name: 'sf-pro-micro'
+
+}
 
 class COMHelper {
 
@@ -25,5 +29,23 @@ class COMHelper {
             });
         });
     }
+
+        
+
+    /**
+     * Attempt to upload some Code
+     */
+    static uploadToDevice(device,data)
+    {
+        return new Promise((resolve, reject) => {
+            //Might use Avrgirl library to upload to device
+
+
+            //Convert data to a hex file
+
+            //push to device
+        });
+    }
+
 }
 module.exports = COMHelper
