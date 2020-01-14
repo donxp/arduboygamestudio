dea324b3727b53513d04087efa179f9017be2e8c
Respects <rektlikeshrek@gmail.com>
2020-01-14 21:51:15 +0000

    Added some basics to preferences.js

--- app/components/Preferences.js
+++ app/components/Preferences.js
@@ -1,11 +1,42 @@
 let COMHelper = require('../util/COMHelper')
 
-VTTCue.component('preferences', {
+Vue.component('preferences', {
     template: `
-    <div>
-        <select name="COMPorts">
-        <option value="x">x</option>
-        </select>
+    <div id="preferences-modal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Preferences</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    Close
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <h5>COM Port Selection</h5>
+                    <select id="COMPorts">
+                    <option value="x">x</option>
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
     `,//makes the template for page
+    data: function() {
+        return {
+            ports: [],
+            selectedPort: ''
+        }
+    }, 
+    methods: {
+        importPorts: function() {
+            this.ports = COMHelper.checkForPorts();
+            
+        },
+
+
+    }
+
 })
+
+module.exports = Preferences
