872b611161b593ca8e14d7d7171fe530140a2917
tsdh2 <tsdh2@kent.ac.uk>
2020-02-08 18:01:30 +0000

    relative paths began being added

--- app/components/ProjectActions.js
+++ app/components/ProjectActions.js
@@ -3,6 +3,10 @@ let ProjectManager = require('../util/ProjectManager.js')
 let AsyncFileHelper = require('../util/AsyncFileHelper.js')
 let Preferences = require('../components/Preferences.js')
 
+let {spawn} = require('child_process')
+
+
+
 Vue.component('project-actions', {
     template: `
     <div>
@@ -59,15 +63,35 @@ Vue.component('project-actions', {
             })
         },
         verifyProject: function() {
-            var comPort = Preferences.getPort();//used when retrieving com port
+            //
+//C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\arduino-builder -dump-prefs -logger=machine -hardware C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\hardware -hardware C:\Users\thoma\Documents\ArduinoData\packages -tools C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\tools-builder -tools C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\hardware\tools\avr -tools C:\Users\thoma\Documents\ArduinoData\packages -built-in-libraries C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\libraries -libraries C:\Users\thoma\Documents\Arduino\libraries -fqbn=arduboy-homemade:avr:arduboy-homemade:based_on=promicro_alt,boot=cathy3k,core=arduboy-core,display=sh1106,flashselect=rx -ide-version=10811 -build-path C:\Users\thoma\AppData\Local\Temp\arduino_build_372733 -warnings=none -build-cache C:\Users\thoma\AppData\Local\Temp\arduino_cache_143338 -prefs=build.warn_data_percentage=75 -verbose C:\Users\thoma\AppData\Local\Temp\untitled1267264915.tmp\sketch_feb08a\sketch_feb08a.ino
+//C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\arduino-builder -compile -logger=machine -hardware C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\hardware -hardware C:\Users\thoma\Documents\ArduinoData\packages -tools C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\tools-builder -tools C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\hardware\tools\avr -tools C:\Users\thoma\Documents\ArduinoData\packages -built-in-libraries C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt\libraries -libraries C:\Users\thoma\Documents\Arduino\libraries -fqbn=arduboy-homemade:avr:arduboy-homemade:based_on=promicro_alt,boot=cathy3k,core=arduboy-core,display=sh1106,flashselect=rx -ide-version=10811 -build-path C:\Users\thoma\AppData\Local\Temp\arduino_build_372733 -warnings=none -build-cache C:\Users\thoma\AppData\Local\Temp\arduino_cache_143338 -prefs=build.warn_data_percentage=75 -verbose C:\Users\thoma\AppData\Local\Temp\untitled1267264915.tmp\sketch_feb08a\sketch_feb08a.ino
+            var buildpath = '';
+            var arduino = 'C:\Program Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.29.0_x86__mdqgnx93n4wtt';
+            var arduinobuilder = arduino + '\arduino-builder';
+            var arduinohardware = arduino + '\hardware';
+            var arduinohardware2 = 'C:\Users\thoma\Documents\ArduinoData\packages';
+            var arduinotools = arduino + '\toolsbuilder';
+            var arduinotools2 = arduinohardware + '\tools\avr';
+            var arduinotools3 = arduinohardware2;
+            var arduinolibraries = arduino + '\libraries';
+            var fqbn = 'arduboy-homemade:avr:arduboy-homemade:based_on=promicro_alt,boot=cathy3k,core=arduboy-core,display=sh1106,flashselect=rx';
+            var command = '';
             //process for compiling to hex
-
-            //process for then uploading to board using set Port
-
+            spawn(command, function (error,stdout,stderr) {
+                console.log(stdout);
+            });
             
+
+
         },
         uploadProject: function() {
-
+            var comPort = Preferences.getPort();
+            if (comPort == '') {
+                alert("Please select a Port in Preferences!");
+            }
+            //process for then uploading to board using set Port
+            
         }
     }
 })
