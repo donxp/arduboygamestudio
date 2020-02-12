330431f11390132d149d5ced497e05aa5611cc8f
D. Paplauskas <dp404@kent.ac.uk>
2020-02-12 20:28:37 +0000

    editing sprite

--- app/components/SpriteCreator.js
+++ app/components/SpriteCreator.js
@@ -11,7 +11,7 @@ Vue.component('sprite-creator', {
 				<div class="sprite" v-for="(item, index) in sprites">
 					<div class="row">
 					<div class="col">{{ item.name }}</div>
-					<div class="col-3 pr-1"><button class="btn btn-sm btn-warning btn-block" @click="rename(index)">Rename</button></div>
+					<div class="col-3 pr-1"><button class="btn btn-sm btn-warning btn-block" @click="edit(index)">Edit</button></div>
 					<div class="col-3 pl-1"><button class="btn btn-sm btn-danger btn-block" @click="remove(index)">Remove</button></div>
 					</div>
 				</div>
@@ -29,19 +29,26 @@ Vue.component('sprite-creator', {
 				</div>
 				<div class="modal-body">
 				<div class="row">
-					<div class="col-sm-3 offset-sm-3">
-						Sprite width: {{ creatorWidth }}
-						<input v-model="creatorWidth" min="8" max="64" type="range">
+					<div class="col-sm-4 d-flex align-items-center">
+						<input v-model="newSpriteName" type="text" class="form-control form-control-sm" placeholder="Name">
 					</div>
-					<div class="col-sm-3">
-						Sprite height: {{ creatorHeight }}
-						<input v-model="creatorHeight" min="8" step="8" max="64" type="range">
+					<div class="col-sm-8">
+						<div class="row">
+							<div class="col-sm-6">
+								Width: {{ creatorWidth }}
+								<input v-model="creatorWidth" min="8" max="64" type="range">
+							</div>
+							<div class="col-sm-6">
+								Height: {{ creatorHeight }}
+								<input v-model="creatorHeight" min="8" step="8" max="64" type="range">
+							</div>
+						</div>
 					</div>
 				</div>
-				<canvas ref="spriteCreator" width="766" height="766" @click="spriteCreatorCanvasClick($event)"></canvas>
+				<canvas class="mt-3" ref="spriteCreator" width="766" height="766" @click="spriteCreatorCanvasClick($event)"></canvas>
 				</div>
 				<div class="modal-footer">
-				<button type="button" class="btn btn-primary" @click="spriteCreatorSave">Save</button>
+				<button type="button" class="btn btn-primary" @click="save" :disabled="newSpriteName.length < 3">Save</button>
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
 				</div>
 			</div>
@@ -52,43 +59,75 @@ Vue.component('sprite-creator', {
 	props: ['shown'],
 	data: function() {
 		return {
-			sprites: [
-				{
-					name: 'test'
-				}
-			],
+			sprites: [],
 			creatorWidth: 8,
 			creatorHeight: 8,
-			image: []
+			image: [],
+			newSpriteName: '',
+			editMode: false,
+			editingIdx: -1,
+			spriteDimensionWatcher: true
 		}
 	},
 	watch: {
-		creatorWidth: function() {
-			this.recreateArray()
-			this.render()
+		creatorWidth: {
+			immediate: true,
+			handler() {
+				console.log(this.spriteDimensionWatcher)
+				if(this.spriteDimensionWatcher) {
+					this.recreateArray()
+					this.render()
+				}
+			}
 		},
-		creatorHeight: function() {
-			this.recreateArray()
-			this.render()
+		creatorHeight: {
+			immediate: true,
+			handler() {
+				console.log(this.spriteDimensionWatcher)
+				if(this.spriteDimensionWatcher) {
+					this.recreateArray()
+					this.render()
+				}
+			}
 		}
 	},
 	methods: {
 		addSprite() {
+			this.editMode = false
+			this.editingIdx = -1
+
 			this.resetImage()
 			this.recreateArray()
+
+			this.creatorWidth = 8
+			this.creatorHeight = 8
+			this.newSpriteName = ''
+
 			$('#sprite-creator-modal').modal('show')
 			this.render()
 		},
-		rename(index) {
-			Dialog.prompt('New name:').then(name => {
-				if(!name) return
-				name = name.trim()
-				if(!this.validateSpriteName(name)) {
-					Dialog.alert('Invalid new sprite name.')
-				} else {
-					this.sprites[index].name = name
-				}
+		edit(index) {
+			this.editMode = true
+			this.editingIdx = index
+
+			this.spriteDimensionWatcher = false
+
+			this.$nextTick(() => {
+				this.creatorWidth = this.sprites[index].image.length
+				console.log('set width:', this.creatorWidth)
+				this.creatorHeight = this.sprites[index].image[0].length
+				console.log('set height:', this.creatorHeight)
+	
+				//this.spriteDimensionWatcher = true
+	
+				console.log('set image to', this.sprites[index].image)
+				this.image = this.sprites[index].image
 			})
+			
+			this.newSpriteName = this.sprites[index].name
+
+			$('#sprite-creator-modal').modal('show')
+			this.render()
 		},
 		remove(index) {
 			Dialog.confirm('Are you sure?').then(confirm => {
@@ -103,6 +142,7 @@ Vue.component('sprite-creator', {
 			return true
 		},
 		recreateArray() {
+			console.log('array recreated')
 			// create colums
 			for(let i = 0; i < this.creatorWidth; i++) {
 				this.image[i] = []
@@ -110,49 +150,46 @@ Vue.component('sprite-creator', {
 					this.image[i][k] = false
 				}
 			}
-			console.log(this.image)
+			// console.log(this.image)
 		},
 		resetImage() {
 			this.image = []
 		},
 		render() {
-            const ctx = this.$refs.spriteCreator.getContext('2d')
-            
-            const width = this.$refs.spriteCreator.width
-            const height = this.$refs.spriteCreator.height
+			if(!this.$refs.spriteCreator) return
+			const ctx = this.$refs.spriteCreator.getContext('2d')
+				
+			const width = this.$refs.spriteCreator.width
+			const height = this.$refs.spriteCreator.height
 
-            ctx.clearRect(0, 0, width, height);
-            // draw width lines
-            const lineWidth = width / this.creatorWidth
+			ctx.clearRect(0, 0, width, height);
+			// draw width lines
+			const lineWidth = width / this.creatorWidth
 
-            for(let i = 1; i <= this.creatorWidth; i++) {
-                // this.spriteCreatorImage.length = this.spriteCreatorWidth
-                // if(this.spriteCreatorImage[i-1] === undefined) {
-                //     this.spriteCreatorImage[i-1] = []
-                // }
-                if(i === this.creatorWidth) {
-                    break
-                }
-                let curX = lineWidth * i
-                ctx.beginPath()
-                ctx.moveTo(curX, 0)
-                ctx.lineTo(curX, height)
-                ctx.stroke()
-            }
-
-            // draw height lines
-            const lineHeight = height / this.creatorHeight
-            for(let i = 1; i <= this.creatorHeight; i++) {
-                if(i === this.creatorHeight) {
-                    break
-                }
-                let curY = lineHeight * i
-                ctx.beginPath()
-                ctx.moveTo(0, curY)
-                ctx.lineTo(width, curY)
-                ctx.stroke()
+			for(let i = 1; i <= this.creatorWidth; i++) {
+				if(i === this.creatorWidth) {
+						break
+				}
+				let curX = lineWidth * i
+				ctx.beginPath()
+				ctx.moveTo(curX, 0)
+				ctx.lineTo(curX, height)
+				ctx.stroke()
 			}
-			
+
+			// draw height lines
+			const lineHeight = height / this.creatorHeight
+			for(let i = 1; i <= this.creatorHeight; i++) {
+				if(i === this.creatorHeight) {
+						break
+				}
+				let curY = lineHeight * i
+				ctx.beginPath()
+				ctx.moveTo(0, curY)
+				ctx.lineTo(width, curY)
+				ctx.stroke()
+			}
+
 			// fill in selected squares
 			for(let col = 0; col < this.creatorWidth; col++) {
 				for(let row = 0; row < this.creatorHeight; row++) {
@@ -164,25 +201,40 @@ Vue.component('sprite-creator', {
 					}
 				}
 			}
-        },
-        spriteCreatorCanvasClick(event) {
-            const canvas = this.$refs.spriteCreator
-            const canvasRect = canvas.getBoundingClientRect()
-            const width = canvas.width
-            const height = canvas.height
-            const x = event.clientX - canvasRect.left
-            const y = event.clientY - canvasRect.top
-
-            const col = Math.ceil(x / (width / this.creatorWidth))
-            const row = Math.ceil(y / (height / this.creatorHeight))
-            this.handleSpriteCreatorGridClick(row, col)
-        },
-        handleSpriteCreatorGridClick(row, col) {
-			console.log('row: ' + row + ' col: ' + col)
+		},
+		spriteCreatorCanvasClick(event) {
+			const canvas = this.$refs.spriteCreator
+			const canvasRect = canvas.getBoundingClientRect()
+			const width = canvas.width
+			const height = canvas.height
+			const x = event.clientX - canvasRect.left
+			const y = event.clientY - canvasRect.top
+						
+			const col = Math.ceil(x / (width / this.creatorWidth))
+			const row = Math.ceil(y / (height / this.creatorHeight))
+			this.handleSpriteCreatorGridClick(row, col)
+		},
+		handleSpriteCreatorGridClick(row, col) {
+			// console.log('row: ' + row + ' col: ' + col)
 			this.image[col-1][row-1] = !this.image[col-1][row-1]
-			console.log(this.image)
+			// console.log(this.image)
 			this.render()
-        },
-		spriteCreatorSave: function() {}
+		},
+		save() {
+			if(this.editMode) {
+				this.sprites[this.editingIdx].name = this.newSpriteName
+				this.sprites[this.editingIdx].image = this.image
+				this.editMode = false
+			} else {
+				this.sprites.push({
+					name: this.newSpriteName,
+					image: this.image
+				})
+			}
+			$('#sprite-creator-modal').modal('hide')
+
+			this.newSpriteName = ''
+			console.log(this.sprites)
+		}
 	}
 })
