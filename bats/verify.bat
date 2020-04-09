8442c785d8c663e9c68558c4b5b6c0430749afed
Don <donshius@inbox.lt>
2020-04-09 22:39:30 +0100

    delete old bats

--- bats/verify.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ../arduino-cli
-echo Compiling
-arduino-cli compile --fqbn arduboy-homemade:avr:arduboy-homemade:based_on=promicro_alt,boot=cathy3k,core=arduboy-core,display=sh1106,flashselect=rx HelloWorld
-pause
